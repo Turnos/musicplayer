45e39ef3da32f126b1a8d7872a2b5adce2606320
walttt <freshmanwb@googlemail.com>
2018-06-24 12:34:07 +0200

    Push1

---
+++ app/src/main/java/de/mse/musicplayer/ArtistsActivity.java
@@ -0,0 +1,4 @@
+package de.mse.musicplayer;
+
+class ArtistsActivity {
+}
--- app/src/main/java/de/mse/musicplayer/MainActivity.java
+++ app/src/main/java/de/mse/musicplayer/MainActivity.java
@@ -1,13 +1,62 @@
 package de.mse.musicplayer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.initializeUI();
+    }
+
+    private void initializeUI(){
         setContentView(R.layout.activity_main);
+
+        //Random button - directs to the player with a playlist with random tracks
+        Button randomButton = this.findViewById(R.id.top_button);
+        randomButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // TODO change the target activity
+                // Intent switcher = new Intent(WelcomeFrame.this, ArtistsActivity.class);
+                // startActivity(switcher);
+            }
+        });
+
+        //Artist button - directs to playlists filtered by artists
+        Button artistButton = this.findViewById(R.id.left_button);
+        artistButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent switcher = new Intent(MainActivity.this, ArtistsActivity.class);
+                startActivity(switcher);
+            }
+        });
+
+        //Playlist button - directs to custom playlists
+        Button playlistButton = this.findViewById(R.id.right_button);
+        playlistButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent switcher = new Intent(MainActivity.this, PlaylistActivity.class);
+                startActivity(switcher);
+            }
+        });
+
+        //Info button - directs to options and help
+        Button infoButton = this.findViewById(R.id.bottom_button);
+        infoButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //TODO change the target activity
+                //Intent switcher = new Intent(WelcomeFrame.this, ArtistsActivity.class);
+                //startActivity(switcher);
+            }
+        });
     }
 }
